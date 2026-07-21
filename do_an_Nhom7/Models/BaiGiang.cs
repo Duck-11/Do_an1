18b8f9d2365097ae229bb075145f6bc5c570a197
lnhuyen2908 <lnhuyen2908@gmail.com>
2026-07-21 17:09:10 +0700

    Tạo model

---
+++ do_an_Nhom7/Models/BaiGiang.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace do_an_Nhom7.Models
+{
+    public class CourseLecture
+    {
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Course))]
+        public int CourseId { get; set; }
+
+        [ForeignKey(nameof(Teacher))]
+        public int TeacherId { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập tiêu đề bài giảng.")]
+        public string Title { get; set; } = string.Empty;
+
+        public string FileName { get; set; } = string.Empty;
+        public string FileUrl { get; set; } = string.Empty;
+        public DateTime UploadedAt { get; set; } = DateTime.Now;
+        public Course? Course { get; set; }
+        public Teacher? Teacher { get; set; }
+    }
+}
--- do_an_Nhom7/obj/Debug/net6.0/do_an_Nhom7.AssemblyInfo.cs
+++ do_an_Nhom7/obj/Debug/net6.0/do_an_Nhom7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("do_an_Nhom7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed89cc80a1d5f3738db6e00807b01795616d21d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("do_an_Nhom7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("do_an_Nhom7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

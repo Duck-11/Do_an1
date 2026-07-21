ed89cc80a1d5f3738db6e00807b01795616d21d6
lnhuyen2908 <lnhuyen2908@gmail.com>
2026-07-21 15:50:12 +0700

    first

---
+++ do_an_Nhom7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace do_an_Nhom7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
